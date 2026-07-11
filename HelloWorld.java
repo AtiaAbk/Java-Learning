f50ce51834a9d75f7b13754142b32bac0b3dbe21
AtiaAbk <atiasanjida1209@gmail.com>
2026-07-11 17:14:04 +0600

    2.1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,8 @@ public class HelloWorld {
         System.out.println("Hello, VS Code!");
         System.out.println("Java is working correctly.");
 
-        System.out.println("Java Version: " + System.getProperty("java.version"));
-        System.out.println("Java Home: " + System.getProperty("java.home"));
-        System.out.println("Operating System: " + System.getProperty("os.name"));
-
         int a = 15;
         int b = 25;
-
         System.out.println("15 + 25 = " + (a + b));
     }
 }
