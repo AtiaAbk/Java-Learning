5207371bbe13a0517e57dcf2895882966340aca3
AtiaAbk <atiasanjida1209@gmail.com>
2026-07-11 20:22:04 +0600

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, VS Code!");
-        System.out.println("Java is working correctly.");
+        System.out.println("hello i am oishi.");
 
         int a = 15;
         int b = 25;
