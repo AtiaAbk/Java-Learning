81d18dacc3afe891791b08035d796ff5251d0937
AtiaAbk <atiasanjida1209@gmail.com>
2026-07-16 00:14:17 +0600

    update

---
+++ atia.java
@@ -0,0 +1,11 @@
+public class Atia {
+
+    // Constructor
+    public Atia() {
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        System.out.println("Program is running!");
+    }
+}
