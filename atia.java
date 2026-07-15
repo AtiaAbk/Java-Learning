3b6e0f6ea3943a61e1a1ad09c06b16c74106d909
AtiaAbk <atiasanjida1209@gmail.com>
2026-07-16 01:41:20 +0600

    update

--- atia.java
+++ atia.java
@@ -5,7 +5,7 @@ public class atia {
     }
 
     // Main method
-    public static void main(String[] args) {
+    public static void main() {
         System.out.println("Program is running!");
     }
 }
