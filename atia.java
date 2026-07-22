2961da2590e1647317e87109b4efd0b863d815ad
AtiaAbk <atiasanjida1209@gmail.com>
2026-07-22 12:38:10 +0600

    extended

--- atia.java
+++ atia.java
@@ -1,11 +1,14 @@
 
-public class atia{
-
-public static void main(String[] args) {
-
-    System.out.println("atia");
+class Net {
+    void ping() {
+        System.out.println("Extending the net...");
+    }
 }
 
+public class atia extends Net {
 
-
+    public static void main(String[] args) {
+        atia app = new atia();
+        app.ping();
+    }
 }
