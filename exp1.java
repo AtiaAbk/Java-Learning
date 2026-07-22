360106adb1344ff6d35ed1bdb0dff2284a82c1a1
AtiaAbk <atiasanjida1209@gmail.com>
2026-07-22 13:53:00 +0600

    lab1 commits for boilerplate setup

--- exp1.java
+++ exp1.java
@@ -4,6 +4,10 @@ public class exp1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!"); 
+
         
 
         sc.close();
--- exp2.java
+++ exp2.java
@@ -1,4 +1,4 @@
-package lab1;
+
 
 public class exp2 {
     
